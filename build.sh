e9cb2c999468d61f3f0fdb810359206e4b025200
gustavo <gamendez98@gmail.com>
2025-03-04 19:04:42 -0500

    some commit

--- build.sh
+++ build.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+docker-compose down
+
 if [ ! -d "rag-api" ]; then
     git clone https://github.com/SantiagoFino/rag-api
+else
+    cd rag-api && git pull && cd ..
 fi
 
 if [ ! -d "cloud-solutions-api" ]; then
     git clone https://github.com/gamendez98/cloud-solutions-api
+else
+    cd cloud-solutions-api && git pull && cd ..
 fi
 
 if [ ! -d "misisCloudProject1-frotend" ]; then
     git clone https://github.com/helberthCO/misisCloudProject1-frotend
+else
+    cd misisCloudProject1-frotend && git pull && cd ..
 fi
 
 docker-compose up --build
