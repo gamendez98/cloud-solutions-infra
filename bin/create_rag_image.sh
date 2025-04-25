74bd506aca7c3bc00d34783a7981ec2dc11468b7
gustavo <gamendez98@gmail.com>
2025-04-24 20:00:13 -0500

    separate rag images

--- bin/create_rag_image.sh
+++ bin/create_rag_image.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-docker build -t us-docker.pkg.dev/proyecto-cs-455920/gcr-compat/rag-api rag-api
-docker push us-docker.pkg.dev/proyecto-cs-455920/gcr-compat/rag-api
+docker build -f rag-api/document_indexing.dockerfile -t us-docker.pkg.dev/proyecto-cs-455920/gcr-compat/rag-api-indexing rag-api
+docker push us-docker.pkg.dev/proyecto-cs-455920/gcr-compat/rag-api-indexing
+
+docker build -f rag-api/ai_assistant.dockerfile -t us-docker.pkg.dev/proyecto-cs-455920/gcr-compat/rag-api-assistant rag-api
+docker push us-docker.pkg.dev/proyecto-cs-455920/gcr-compat/rag-api-assistant
